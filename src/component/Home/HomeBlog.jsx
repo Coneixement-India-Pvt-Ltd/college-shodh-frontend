e9e03f5263193e4aaa337903d449c8d7d2823735
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2024-09-19 17:21:08 +0530

    restructured code

---
+++ src/component/Home/HomeBlog.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BlogListing from "../BlogListing/BlogListing";
+
+const HomeBlog = () => {
+  return (
+    <div className="ml-32 mr-36 min-h-screen flex flex-col">
+      <div className="mt-10 flex flex-row space-x-4 mb-10">
+        <div className="flex-shrink-0 w-72 h-40 bg-gray-200 rounded-md shadow-lg p-4">
+          <BlogListing />
+        </div>
+        <div className="flex-shrink-0 w-72 h-40 bg-gray-200 rounded-md shadow-lg p-4">
+          <BlogListing />
+        </div>
+        <div className="flex-shrink-0 w-72 h-40 bg-gray-200 rounded-md shadow-lg p-4">
+          <BlogListing />
+        </div>
+        <div className="flex-shrink-0 w-72 h-40 bg-gray-200 rounded-md shadow-lg p-4">
+          <BlogListing />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBlog;
