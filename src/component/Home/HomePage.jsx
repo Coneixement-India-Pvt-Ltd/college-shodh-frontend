262aaf493a1a6b04fd9642ae3957841f6908566f
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2024-09-19 17:21:44 +0530

    restructured code

---
+++ src/component/Home/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./Home";
+import Programs from "./Programs";
+import TopCities from "./TopCities";
+import HomeBlog from "./HomeBlog";
+
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+      <Programs />
+      <TopCities />
+      <HomeBlog />
+    </>
+  );
+};
+
+export default HomePage;
